854be7db1c551f89004b233f3493313263491e03
TINNIA <ektha3869@gmail.com>
2021-03-05 16:44:25 +0900

    210305

--- 2018 KAKAO BLIND RECRUITMENT/[1차]뉴스 클러스터링_다솜.py
+++
@@ -1,9 +0,0 @@
-def solution(str1, str2):
-    str1 = [str1[i:i + 2].lower() for i in range(len(str1) - 1) if str1[i:i + 2].isalpha()]
-    str2 = [str2[i:i + 2].lower() for i in range(len(str2) - 1) if str2[i:i + 2].isalpha()]
-
-    if not str1 and not str2:
-        return 65536
-    ans1 = sum([min(str1.count(i), str2.count(i)) for i in list(set(str1) & set(str2))])
-    ans2 = sum([max(str1.count(i), str2.count(i)) for i in list(set(str1) | set(str2))])
-    return int(ans1 / ans2 * 65536)
